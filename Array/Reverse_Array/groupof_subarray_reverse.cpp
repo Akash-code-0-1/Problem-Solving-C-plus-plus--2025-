966dac917107e639079be8771bceb4f5c5a1dd94
Tanvir <akash01algo@gmail.com>
2025-04-09 12:13:55 +0600

    reverse group subarray

---
+++ Array/Reverse_Array/groupof_subarray_reverse.cpp
@@ -0,0 +1,31 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void reverseInGroups(vector<int> &arr, int k)
+{
+    for (int i = 0; i < arr.size(); i += k)
+    {
+        int left = i;
+        int right = std::min(i + k - 1, (int)arr.size() - 1);
+
+        while (left < right)
+        {
+            swap(arr[left++], arr[right--]);
+        }
+    }
+}
+
+int main()
+{
+    vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 8}; // Input array
+    int k = 3;                                  // Size of sub-arrays to be reversed
+
+    reverseInGroups(arr, k);
+
+    for (int num : arr)
+        cout << num << " ";
+
+    return 0;
+}
