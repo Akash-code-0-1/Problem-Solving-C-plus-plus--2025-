fcefb6c0282d59eb78c98748a5d996d896dc56dc
Akash-code-0-1 <akash01algo@gmail.com>
2025-08-15 15:17:57 +0600

    leetcode 1021

--- LEETCODE/1021.cpp
+++ LEETCODE/1021.cpp
@@ -1,39 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    string removeOuterParentheses(const string& s) {
+    string removeOuterParentheses(const string &s)
+    {
         string result;
         int opened = 0;
-        for (char c : s) {
-            if (c == '(') {
-                if (opened > 0) result += c; // not outermost '('
+        for (char c : s)
+        {
+            if (c == '(')
+            {
+                if (opened > 0)
+                    result += c; // not outermost '('
                 opened++;
-            } else { // c == ')'
+            }
+            else
+            { // c == ')'
                 opened--;
-                if (opened > 0) result += c; // not outermost ')'
+                if (opened > 0)
+                    result += c; // not outermost ')'
             }
         }
         return result;
     }
 };
 
-void test(const string& input, const string& expected) {
+void test(const string &input, const string &expected)
+{
     Solution sol;
     string output = sol.removeOuterParentheses(input);
-    if (output == expected) {
+    if (output == expected)
+    {
         cout << "PASS: Input: \"" << input << "\" Output: \"" << output << "\"\n";
-    } else {
+    }
+    else
+    {
         cout << "FAIL: Input: \"" << input << "\" Output: \"" << output << "\" Expected: \"" << expected << "\"\n";
     }
-
-
 }
 
-
-
-int main() {
+int main()
+{
     test("(()())(())", "()()()");
     test("(()())(())(()(()))", "()()()()(())");
     test("()()", "");
