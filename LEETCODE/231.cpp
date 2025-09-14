a355d11ead79907e5c5f64d26c85cb8b914e8aeb
Akash-code-0-1 <akash01algo@gmail.com>
2025-09-14 22:57:05 +0600

    231 leetcode solved

---
+++ LEETCODE/231.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if (n <= 0) return false;
+        return (n & (n - 1)) == 0;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    // Sample inputs
+    vector<int> nums = {1, 2, 3, 4, 5, 8, 16, 18};
+
+    for (int n : nums) {
+        if (sol.isPowerOfTwo(n)) {
+            cout << n << " is a power of 2." << endl;
+        } else {
+            cout << n << " is NOT a power of 2." << endl;
+        }
+    }
+
+    return 0;
+}
