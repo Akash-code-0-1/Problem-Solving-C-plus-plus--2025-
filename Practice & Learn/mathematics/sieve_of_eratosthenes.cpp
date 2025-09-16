27c0a43ec82d2d2c769c9580a12da75291f51ad5
Akash-code-0-1 <akash01algo@gmail.com>
2025-09-16 12:20:27 +0600

    learned sieve of eratosthenes that generate all prime numbers in n

---
+++ Practice & Learn/mathematics/sieve_of_eratosthenes.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> sieve(int n)
+{
+
+    vector<bool> isPrime(n, true);   // intially all set to prime number by setting true
+    isPrime[0] = isPrime[1] = false; // 0,1 are not prime so set those as not not prime by false
+
+    // starting from 2 beacause, we alrady set 0,1 as not prime and we will eliminate multiples so it must be in less than or equle to n
+    for (int i = 2; i * i <= n; i++)
+    {
+        //if current number is true it will eliminate its all multiples
+        if(isPrime[i]){
+            for(int j=i*i;j<=n;j+=i){
+                isPrime[j]=false;
+            }
+        }
+    }
+
+    vector<int>primes;
+    for(int i=2;i<=n;i++){
+        if(isPrime[i]){
+            primes.push_back(i);
+        }
+    }
+
+    return primes;
+}
+
+int main()
+{
+    int n = 30;
+    auto primes = sieve(n);
+    for (int p : primes)
+        cout << p << " ";
+}
