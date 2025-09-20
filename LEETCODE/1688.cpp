1dff97bda9325b38270b1a9a87a13fefcc7bd623
Akash-code-0-1 <akash01algo@gmail.com>
2025-09-21 03:28:44 +0600

    1688 leetcode solved

---
+++ LEETCODE/1688.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        return n-1;
+    }
+};
+
+int main() {
+    int n = 7;
+    Solution sol;
+    cout << sol.numberOfMatches(n) << endl;
+    return 0;
+}
