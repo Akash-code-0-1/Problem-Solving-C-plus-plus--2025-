e3096f2a0bada44a676ac7635967d0ccc34d0220
Akash-code-0-1 <akash01algo@gmail.com>
2025-10-07 22:04:39 +0600

    180 degree toration (clock wise) 2D matrix

---
+++ Practice & Learn/Array/180_degree_2d_matrix_rotation.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<vector<int>> matrix = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    // 180° rotation = reverse rows + reverse each row
+    reverse(matrix.begin(), matrix.end());
+    for (auto &row : matrix)
+        reverse(row.begin(), row.end());
+
+    cout << "180 Degree Rotated Matrix:" << endl;
+    for (const auto &row : matrix)
+    {
+        for (int val : row)
+        {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+}
---
+++ Practice & Learn/Array/tempCodeRunnerFile.cpp
@@ -0,0 +1,14 @@
+    // // Step 1: Transpose
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     for (int j = i; j < 3; j++)
+    //     {
+    //         swap(matrix[i][j], matrix[j][i]);
+    //     }
+    // }
+
+    // // Step 2: Reverse each row
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     reverse(matrix[i].begin(), matrix[i].end());
+    // }
