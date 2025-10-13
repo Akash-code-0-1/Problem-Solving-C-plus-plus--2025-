5714e1a979f8aa00199bec9a84195b7821a76516
Akash-code-0-1 <akash01algo@gmail.com>
2025-10-13 15:37:33 +0600

    solution of leetcode problem 1534

---
+++ LEETCODE/1534.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int countGoodTriplets(vector<int> &arr, int a, int b, int c)
+    {
+
+        int count = 0;
+        for (int i = 0; i < arr.size(); i++)
+        {
+            for (int j = i + 1; j < arr.size(); j++)
+            {
+                for (int k = j + 1; k<arr.size(); k++)
+                {
+                    if (
+                        abs(arr[i] - arr[j]) <= a &&
+                        abs(arr[j] - arr[k]) <= b &&
+                        abs(arr[i] - arr[k]) <= c)
+                    {
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+};
+
+int main()
+{
+    Solution solution;
+
+    vector<int> arr = {3, 0, 1, 1, 9, 7};
+    int a = 7, b = 2, c = 3;
+
+    int result = solution.countGoodTriplets(arr, a, b, c);
+    cout << "Result: " << result << endl;
+
+    return 0;
+}
