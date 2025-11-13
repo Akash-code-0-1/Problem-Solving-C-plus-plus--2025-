c3b727c0b27aef4b46d01786a975ed332747065e
Akash-code-0-1 <akash01algo@gmail.com>
2025-11-13 16:57:50 +0600

    solution of leetcode problem 387

---
+++ LEETCODE/387.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        vector<int> freq(26,0);
+        for(char c: s) freq[c-'a']++;
+
+        for(int i=0;i<s.size();i++){
+            if(freq[s[i]-'a']==1)
+                return i;
+        }
+        return -1;
+    }
+};
+
+int main() {
+    Solution sol;
+    
+    string s1 = "leetcode";
+    cout << sol.firstUniqChar(s1) << endl;
+    
+    string s2 = "loveleetcode";
+    cout << sol.firstUniqChar(s2) << endl;
+    
+    string s3 = "aabb";
+    cout << sol.firstUniqChar(s3) << endl;
+    
+    return 0;
+}
